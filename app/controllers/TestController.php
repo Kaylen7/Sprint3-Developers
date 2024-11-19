c511cd2fbb9cbace5630c868466b90df50bee989
alexperis95 <alexperis95@gmail.com>
2024-11-19 11:13:36 +0100

    feat: :sparkles: Move CRUD to a new individual controller

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -1,26 +1,4 @@
 <?php
 
 class TestController extends ApplicationController
-{
-	public function indexAction()
-	{
-
-		echo "<br/><br/>Obtener todas<br/><br/>";
-		$tasks = $this->database->getAll();
-		var_dump($tasks);
-
-		echo "<br/><br/>Obtener tarea con id '0'<br/><br/>";
-		$task = $this->database->getTask("1");
-		var_dump($task);
-
-		echo "<br/><br/>Editar tarea con id '2'<br/><br/>";
-		$task = $this->database->updateTask("2", ["start_time" => "😨", "description" => "Texto nuevo"]);
-		$task = $this->database->getTask("2");
-		var_dump($task);
-	}
-	
-	public function checkAction()
-	{
-		echo "hello from test::check";
-	}
-}
+{}
