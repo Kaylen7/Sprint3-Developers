7396244917b6498a37fe7ee8405f8620803a7aa5
alexperis95 <alexperis95@gmail.com>
2024-11-22 20:02:33 +0100

    feat: :construction: creating view task

--- app/controllers/ViewController.php
+++ app/controllers/ViewController.php
@@ -7,8 +7,29 @@ class ViewController extends ApplicationController{
         $this->view->tasks = $tasks;
     }
 
-    public function taskAction(){
-        $id = $this->_namedParameters['id'];
-        $this->view->task = $this->database->getTask($id);
+    public function taskAction()
+    {
+      $id = $this->_namedParameters['id'] ?? null;
+      if ($id === null) {
+        $this->renderView('error', ['message' => "El parámetro 'id' es obligatorio."]);
+        return;
+    }
+    
+        $task = $this->database->getTask($id);
+  
+        if (empty($task)) {
+          $this->renderView('error', ['message' => "La tarea no existe."]);
+          return;
+      }      
+      $this->renderView('task', ['task' => $task]);
+    }
+    
+    protected function renderView(string $view, array $data = []) {
+      extract($data);
+
+      $viewPath = realpath(__DIR__ . "/../views/scripts/view/$view.phtml");
+      if ($viewPath && file_exists($viewPath)) {
+          include $viewPath;
+      }
     }
 }
