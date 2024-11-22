b083ee4a1cc07ca799c495c23aff06db9553c43f
Sara DM <ochuck.sdm@gmail.com>
2024-11-22 11:40:31 +0100

    perf: :wheelchair: Added aria-pressed visual changes and improved accessibility visuals for side menu buttons.
    💅 I also cleaned html code a bit up, removed redundant group classes, implemented peer class between button-label relations. Added the corresponding classes in styles.css.

--- web/javascripts/view.js
+++ web/javascripts/view.js
@@ -1,20 +1,30 @@
 const btns = document.getElementsByTagName('button');
 const taskGrid = document.getElementById('task-grid');
 const tasks = taskGrid.querySelectorAll('div[data-state]');
-const search = document.getElementById('search');
+let previouslyPressed = document.querySelector('div[aria-pressed="true"]');
 
 for(let i = 0; i < btns.length; i++){
     btns[i].addEventListener('click', () => {
     if(btns[i].id === 'all'){
         const allTasksHTML = [...tasks].map(task => task.outerHTML).join('');
         taskGrid.innerHTML = allTasksHTML;
+        btns[i].parentNode.setAttribute('aria-pressed', 'true');
     } else {
         const filtered = [...tasks].filter(task => task.getAttribute('data-state') === btns[i].id);
         const filteredHTML = filtered.map(task => task.outerHTML).join('');
         taskGrid.innerHTML = filteredHTML;
+        
+        btns[i].parentNode.setAttribute('aria-pressed', 'true');
     }
+    if(previouslyPressed){
+        previouslyPressed.setAttribute('aria-pressed', 'false');
+    }
+    previouslyPressed = btns[i].parentNode;
+    
     })
 }
+
+const search = document.getElementById('search');
 
 search.addEventListener('keypress', (e) => {
     if(e.key === 'Enter' && e.target.value != ''){
