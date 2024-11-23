d17705fd3d24cd6c0d8f63f982fdbe4d0a214ab0
alexperis95 <alexperis95@gmail.com>
2024-11-23 11:47:35 +0100

    refactor: :recycle: Create stateComponent, moving components and refactor my bad code

--- app/controllers/ViewController.php
+++ app/controllers/ViewController.php
@@ -1,6 +1,6 @@
 <?php
 
-class ViewController extends ApplicationController{
+class ViewController extends ApplicationController {
 
     public function indexAction(){
         $tasks = $this->database->getAll();
@@ -9,15 +9,8 @@ class ViewController extends ApplicationController{
 
     public function taskAction()
     {
-      $id = $this->_namedParameters['id'] ?? null;
-      $task = $this->database->getTask($id);  
-
-      extract([
-        'task' => $task,
-        'sidebarIncluded' => true, //My sidebar was duplicated, with this line I am avoiding it
-    ]);
-
-    include __DIR__ . "/../views/scripts/view/task.phtml";
+        $id = $this->_namedParameters['id'] ?? null;
+        $task = $this->database->getTask($id);
+        $this->view->task = $task;
     }
-
 }
