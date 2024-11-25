a9002d5d2cebcb3bfef44767094d7782c729670b
Sara DM <ochuck.sdm@gmail.com>
2024-11-25 19:05:10 +0100

    feat: :building_construction: Updated MVC of Update path
    Changed view to use viewTaskComponent. Fixed bugs in Database isValidContent and updateInnerTasks, to prevent timestamp to be parsed into string. Added boolean return to updateTask for error handling. Updated Controller

--- app/controllers/EditController.php
+++ app/controllers/EditController.php
@@ -2,12 +2,30 @@
 
 class EditController extends ApplicationController {
     public function updateAction(){
-        if($this->getRequest()->isPost()){
-            echo "Post requested";
-        }
-
         $id = $this->_namedParameters['id'];
         $task = $this->database->getTask($id);
         $this->view->task = $task;
+
+        if($this->getRequest()->isPost()){
+            $request = $this->getRequest();
+            $task = $request->getAllParams();
+
+            $task['start_time'] = strtotime($request->getParam('start_time'));
+            $task['end_time'] = strtotime($request->getParam('end_time'));
+
+            if($task['start_time'] > $task['end_time']){
+                echo $this->showModal('Error!', 'La data de inici no pot ser posterior a la data de final', 'Torna');
+                return;
+            }
+
+            $update = $this->database->updateTask($this->_namedParameters['id'], $task);
+            if(!$update){
+                echo $this->showModal('Ep!', 'No hi ha canvis', 'Torna');
+                return;
+            }
+            echo $this->showModal('Tasca editada!', 'S\'ha editat correctament!', 'Veure','/view' . '/' . $this->_namedParameters['id']);
+            return;
+            
+        }
     }
 }
