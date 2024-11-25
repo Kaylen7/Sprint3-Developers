5790a0afa279bcd1e090c1a5716249b979ef98b7
Sara DM <ochuck.sdm@gmail.com>
2024-11-25 11:44:29 +0100

    feat: :sparkles: added edit view information to base udpate.phtml

--- app/controllers/EditController.php
+++ app/controllers/EditController.php
@@ -2,7 +2,12 @@
 
 class EditController extends ApplicationController {
     public function updateAction(){
+        if($this->getRequest()->isPost()){
+            echo "Post requested";
+        }
+
         $id = $this->_namedParameters['id'];
         $task = $this->database->getTask($id);
+        $this->view->task = $task;
     }
 }
